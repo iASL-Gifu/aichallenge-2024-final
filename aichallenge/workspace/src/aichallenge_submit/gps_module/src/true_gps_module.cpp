2fb8001ec5851a989ede7e7988f0eab3a8e3b2c8
knorrrr <kunotomoki61@gmail.com>
2024-10-08 20:16:16 +0900

    parameterized gnss module cov

--- aichallenge/workspace/src/aichallenge_submit/gps_module/src/true_gps_module.cpp
+++ aichallenge/workspace/src/aichallenge_submit/gps_module/src/true_gps_module.cpp
@@ -12,7 +12,12 @@ public:
         // パラメータの宣言とデフォルト値
         this->declare_parameter<bool>("debug", false);
         debug_ = this->get_parameter("debug").as_bool();
-
+        scale_pos_cov_x = this->declare_parameter<float>("scale_position_covariance_x");
+        scale_pos_cov_y = this->declare_parameter<float>("scale_position_covariance_y");
+        scale_pos_cov_z = this->declare_parameter<float>("scale_position_covariance_z");
+        scale_ori_cov_0 = this->declare_parameter<float>("scale_orientation_covariance_0");
+        scale_ori_cov_1 = this->declare_parameter<float>("scale_orientation_covariance_1");
+        scale_ori_cov_2 = this->declare_parameter<float>("scale_orientation_covariance_2");
         // サブスクリプションの設定
         subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
             "~/input/pose_with_covariance", 10, std::bind(&PoseComparisonNode::pose_callback, this, std::placeholders::_1));
@@ -28,6 +33,12 @@ private:
     // 最新の2つのメッセージを保持するためのキュー
     std::deque<geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr> pose_queue_;
     bool debug_;  // デバッグフラグ
+    float scale_pos_cov_x;
+    float scale_pos_cov_y;
+    float scale_pos_cov_z;
+    float scale_ori_cov_0;
+    float scale_ori_cov_1;
+    float scale_ori_cov_2;
 
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr publisher_pose_with_cov_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_pose_;
@@ -73,14 +84,14 @@ private:
             auto new_pose_modify_cov = std::make_shared<geometry_msgs::msg::PoseWithCovarianceStamped>(*new_pose);
             new_pose_modify_cov->header.stamp = this->now();
             //Pose Cov
-            new_pose_modify_cov->pose.covariance[0] = new_pose->pose.covariance[0] * 0.1;            
-            new_pose_modify_cov->pose.covariance[7] = new_pose->pose.covariance[7] * 0.1;
+            new_pose_modify_cov->pose.covariance[0] = new_pose->pose.covariance[0] * scale_pos_cov_x;            
+            new_pose_modify_cov->pose.covariance[7] = new_pose->pose.covariance[7] * scale_pos_cov_y;
             // Keep Z 0.0
-            // new_pose_modify_cov->pose.covariance[14] = new_pose->pose.covariance[14] / 10.0;
+            // new_pose_modify_cov->pose.covariance[14] = new_pose->pose.covariance[14] * scale_pos_cov_z;
             // Orientation Cov, nature 0.1 0.1 1.0
-            new_pose_modify_cov->pose.covariance[22] = new_pose->pose.covariance[22] * 10.0;       
-            new_pose_modify_cov->pose.covariance[29] = new_pose->pose.covariance[29] * 10.0;
-            new_pose_modify_cov->pose.covariance[35] = new_pose->pose.covariance[35] * 10.0;
+            new_pose_modify_cov->pose.covariance[22] = new_pose->pose.covariance[22] * scale_ori_cov_0;       
+            new_pose_modify_cov->pose.covariance[29] = new_pose->pose.covariance[29] * scale_ori_cov_1;
+            new_pose_modify_cov->pose.covariance[35] = new_pose->pose.covariance[35] * scale_ori_cov_2;
 
 
             publisher_pose_with_cov_->publish(*new_pose_modify_cov);
